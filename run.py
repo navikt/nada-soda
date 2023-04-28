e9382aa7b03adfec19731a34a47c96fcb56b2a75
Erik Vattekar <erik.vattekar@nav.no>
2023-04-28 12:47:23 +0200

    write soda results to kafka topic

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import os
+import json
 import yaml
 import logging
+import hashlib
+from datetime import datetime
 from soda.scan import Scan
 from soda.execution.check.metric_check import MetricCheck
 from slack_sdk import WebClient
+from kafka import KafkaProducer
 
 class NadaSoda:
     def __init__(
@@ -11,18 +15,31 @@ class NadaSoda:
         soda_config: str, 
         soda_checks_folder: str, 
         slack_channel : str, 
-        slack_token: str
+        slack_token: str,
+        kafka_topic: str
     ) -> None:
         self._soda_config = soda_config
         self._soda_checks_folder = soda_checks_folder
         self._slack_channel = slack_channel if slack_channel.startswith("#") else "#"+slack_channel
         self._slack_token = slack_token
+        self._kafka_topic = kafka_topic
+
+        self._kafka_producer = KafkaProducer(
+            bootstrap_servers=os.environ["KAFKA_BROKERS"],
+            security_protocol="SSL",
+            ssl_cafile=os.environ["KAFKA_CA_PATH"],
+            ssl_certfile=os.environ["KAFKA_CERTIFICATE_PATH"],
+            ssl_keyfile=os.environ["KAFKA_PRIVATE_KEY_PATH"],
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
 
     def run(self) -> None:
         for f in os.listdir(self._soda_checks_folder):
             gcp_project, dataset, scan = self._run_scan(f)
             if scan.has_check_warns_or_fails():
                 self._post_slack(gcp_project, dataset, scan.get_checks_warn_or_fail())
+
+            self._write_to_topic(gcp_project, dataset, scan)
 
     def _run_scan(self, f: str) -> tuple[str, str, Scan]:
         s = Scan()
@@ -114,6 +131,31 @@ class NadaSoda:
                                 blocks=blocks,
                                 attachments=attachments)
 
+    def _write_to_topic(self, gcp_project: str, dataset: str, scan: Scan) -> None:
+        for res in scan.get_scan_results()["checks"]:
+            key, message = self._create_message(gcp_project, dataset, res)
+            self._kafka_producer.send(self._kafka_topic, key=key, value=message)
+
+    def _create_message(self, gcp_project: str, dataset: str, res: dict) -> tuple[str, dict]:
+        message_key = self._create_message_key(gcp_project, dataset, res)
+        test_ts = datetime.now().isoformat()
+        return message_key, {
+            "project": gcp_project,
+            "dataset": dataset,
+            "table": res["table"],
+            "test": res["name"],
+            "definition": res["definition"],
+            "metrics": res["metrics"],
+            "outcome": res["outcome"],
+            "test_ts": test_ts,
+            "resourceAttributes": res.get("resourceAttributes"),
+            "column": res.get("column"),
+            "type": res.get("type"),
+            "filter": res.get("filter")
+        }
+
+    def _create_message_key(self, gcp_project: str, dataset: str, res: dict) -> str:
+        return hashlib.md5(f"{gcp_project}.{dataset}.{res['table']}-{res['name']}".encode()).hexdigest()
 
 if __name__ == "__main__":
     try:
@@ -121,6 +163,7 @@ if __name__ == "__main__":
         checks_path = os.environ["SODA_CHECKS_FOLDER"]
         slack_channel = os.environ["SLACK_CHANNEL"]
         slack_token = os.environ["SLACK_TOKEN"]
+        kafka_topic = os.getenv("KAFKA_TOPIC", "nada.nada-soda")
     except KeyError:
         logging.error("Environment variables SODA_CONFIG, SODA_CHECKS_FOLDER, SLACK_CHANNEL and SLACK_TOKEN are all required to run this script")
         exit(1)
