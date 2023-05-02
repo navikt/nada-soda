847395c71344a8032eb9ad1ba0daeca3d01ac5bf
Erik Vattekar <erik.vattekar@nav.no>
2023-05-02 14:40:53 +0200

    results are now written to soda-api

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# NADA soda
+# NADA Soda
 Naisjob example for periodically running [soda](https://github.com/sodadata/soda-core) checks against BigQuery tables and posting data quality errors to slack.
 
 ## Prerequisites
-The python script requires 
+The python script requires
 
 - A [soda config](https://docs.soda.io/soda/connect-bigquery.html#connection-configuration) for connecting to one or several BigQuery datasource(s). See [config example](https://github.com/navikt/nada-soda/blob/main/.local/soda-config/config.yaml) for configuring this for nais.
 - One or several [soda check config(s)](https://docs.soda.io/soda-cl/soda-cl-overview.html) describing the checks you want to perform on the BigQuery tables. See [config folder](https://github.com/navikt/nada-soda/tree/main/.local/soda-checks) for examples.
@@ -11,7 +11,7 @@ The python script requires
 
 All of the above requirements must be configured with environment variables as described in [Required environment variables](#required-environment-variables) below.
 
-The soda config and the soda check files needs to be mounted into the container environment on nais. In this example these configurations files are mounted from configmaps deployed together with the naisjob to the cluster. See [.nais folder](https://github.com/navikt/nada-soda/tree/main/.nais) for example on how to do this. In this example the slack token is set as an [environment variable from a secret](https://github.com/navikt/nada-soda/blob/main/.nais/naisjob.yaml#L30) in the cluster deployed manually where the key in the secret is `SLACK_TOKEN`.
+The soda config and the soda check files needs to be mounted into the container environment on nais. In this example these configurations files are mounted from configmaps deployed together with the naisjob to the cluster. See [.nais folder](https://github.com/navikt/nada-soda/tree/main/.nais) for example on how to do this.
 
 You will also need additional [project level iam roles](https://github.com/navikt/nada-soda/blob/main/.nais/naisjob.yaml#L32-L47) for the naisjob service account in order to be allowed to perform the soda checks.
 
@@ -21,7 +21,6 @@ Note: The script requires that a datasource name matches the file name for the c
 - `SODA_CONFIG`: Path to soda config file
 - `SODA_CHECKS_FOLDER`: Path to folder containing soda check files for BigQuery datasets
 - `SLACK_CHANNEL`: Desired slack channel for posting data quality errors
-- `SLACK_TOKEN`: Token for the slack app used to post error messages
 
 ## Build and push image
 ````bash
--- run.py
+++ run.py
@@ -1,46 +1,26 @@
 import os
-import json
 import yaml
 import logging
-import hashlib
+import requests
 from datetime import datetime
 from soda.scan import Scan
-from soda.execution.check.metric_check import MetricCheck
-from slack_sdk import WebClient
-from kafka import KafkaProducer
 
 class NadaSoda:
     def __init__(
         self,
         soda_config: str, 
-        soda_checks_folder: str, 
-        slack_channel : str, 
-        slack_token: str,
-        kafka_topic: str
+        soda_checks_folder: str,
+        slack_channel : str
     ) -> None:
         self._soda_config = soda_config
         self._soda_checks_folder = soda_checks_folder
         self._slack_channel = slack_channel if slack_channel.startswith("#") else "#"+slack_channel
-        self._slack_token = slack_token
-        self._kafka_topic = kafka_topic
-
-        self._kafka_producer = KafkaProducer(
-            bootstrap_servers=os.environ["KAFKA_BROKERS"],
-            security_protocol="SSL",
-            ssl_cafile=os.environ["KAFKA_CA_PATH"],
-            ssl_certfile=os.environ["KAFKA_CERTIFICATE_PATH"],
-            ssl_keyfile=os.environ["KAFKA_PRIVATE_KEY_PATH"],
-            value_serializer=lambda v: json.dumps(v).encode('utf-8')
-        )
 
     def run(self) -> None:
         for f in os.listdir(self._soda_checks_folder):
             if f.endswith(".yaml"):
                 gcp_project, dataset, scan = self._run_scan(f)
-                if scan.has_check_warns_or_fails():
-                    self._post_slack(gcp_project, dataset, scan.get_checks_warn_or_fail())
-
-                self._write_to_topic(gcp_project, dataset, scan)
+                self._publish_results(gcp_project, dataset, scan)
 
     def _run_scan(self, f: str) -> tuple[str, str, Scan]:
         s = Scan()
@@ -60,87 +40,14 @@ class NadaSoda:
 
         raise KeyError(f"dataset {dataset} not found in config")
 
-    def _post_slack(self, gcp_project: str, dataset: str, discrepancies: list[MetricCheck]) -> None:
-        discrepancies_unpacked = self._unpack_and_remove_duplicates(discrepancies)
-        errors, warnings = self._separate_errors_and_warnings(discrepancies_unpacked)
+    def _publish_results(self, gcp_project: str, dataset: str, scan: Scan):
+        results = [self._create_soda_result(gcp_project, dataset, r) for r in scan.get_scan_results().get("checks")]
+        res = requests.post("https://nada-soda.intern.dev.nav.no/soda/new", json=results)
+        res.raise_for_status()
 
-        top_block = self._create_slack_block()
-        error_attachment = self._create_error_slack_attachment(gcp_project, dataset, errors) if len(errors) > 0 else None
-        warning_attachment = self._create_warning_slack_attachment(gcp_project, dataset, errors) if len(warnings) > 0 else None
-        self._post_slack_message([top_block], [error_attachment, warning_attachment])
-
-    def _create_slack_block(self) -> dict:
+    def _create_soda_result(self, gcp_project: str, dataset: str, res: dict) -> dict:
         return {
-            "type": "section",
-            "text": {
-                "type": "plain_text",
-                "text": "Varsel om datakvalitetsavvik :gasp:"
-            }
-        }
-
-    def _unpack_and_remove_duplicates(self, discrepancies: list[MetricCheck]) -> list[dict]:
-        out = {}
-        for e in discrepancies:
-            err_dict = e.get_dict()
-            if err_dict["identity"] not in out.keys():
-                out[err_dict["identity"]] = err_dict
-        return [out[key] for key in out.keys()]
-
-    def _separate_errors_and_warnings(self, discrepancies: list[dict]) -> tuple[list[dict], list[dict]]:
-        errors = []
-        warnings = []
-        for d in discrepancies:
-            if d["outcome"] == "fail":
-                errors.append(d)
-            else:
-                warnings.append(d)
-
-        return errors, warnings
-
-    def _create_error_slack_attachment(self, gcp_project: str, dataset: str, errors: list[dict]) -> dict:
-        content = "\n".join([self._create_test_result(e) for e in errors])
-        return {
-            "fallback": "Tester med feil",
-            "color": "#ff2d00",
-            "author_name": f"{gcp_project}.{dataset}",
-            "title": "Tester som feiler:",
-            "text": content,
-            "footer": "SODA Bot"
-        }
-
-    def _create_warning_slack_attachment(self, gcp_project: str, dataset: str, warnings: list[dict]) -> dict:
-        content = "\n".join([self._create_test_result(e) for e in warnings])
-        return {
-            "fallback": "Tester med varslinger",
-            "color": "#ffa500",
-            "author_name": f"{gcp_project}.{dataset}",
-            "title": "Tester med varslinger:",
-            "text": content,
-            "footer": "SODA Bot"
-        }
-
-    def _create_test_result(self, e: dict) -> str:
-        return "\n".join([
-            f"_*Tabell: {e['table']}*_" + (f" _*kolonne: {e.get('column')}*_" if e.get('column') else ""), 
-            f"Test: {e['name']}"
-        ])
-
-    def _post_slack_message(self, blocks: list, attachments: list) -> None:
-        client = WebClient(token=self._slack_token)
-        client.chat_postMessage(channel=self._slack_channel,
-                                text="Datakvalitetsavvik",
-                                blocks=blocks,
-                                attachments=attachments)
-
-    def _write_to_topic(self, gcp_project: str, dataset: str, scan: Scan) -> None:
-        for res in scan.get_scan_results()["checks"]:
-            key, message = self._create_message(gcp_project, dataset, res)
-            self._kafka_producer.send(self._kafka_topic, key=key.encode(), value=message)
-
-    def _create_message(self, gcp_project: str, dataset: str, res: dict) -> tuple[str, dict]:
-        message_key = self._create_message_key(gcp_project, dataset, res)
-        test_ts = datetime.now().isoformat()
-        return message_key, {
+            "id": res["identity"],
             "project": gcp_project,
             "dataset": dataset,
             "table": res["table"],
@@ -148,28 +55,24 @@ class NadaSoda:
             "definition": res["definition"],
             "metrics": res["metrics"],
             "outcome": res["outcome"],
-            "test_ts": test_ts,
+            "time": datetime.now().isoformat(),
             "resourceAttributes": res.get("resourceAttributes"),
             "column": res.get("column"),
             "type": res.get("type"),
-            "filter": res.get("filter")
+            "filter": res.get("filter"),
+            "slack": self._slack_channel
         }
-
-    def _create_message_key(self, gcp_project: str, dataset: str, res: dict) -> str:
-        return hashlib.md5(f"{gcp_project}.{dataset}.{res['table']}-{res['name']}".encode()).hexdigest()
 
 if __name__ == "__main__":
     try:
         config_path = os.environ["SODA_CONFIG"]
         checks_path = os.environ["SODA_CHECKS_FOLDER"]
         slack_channel = os.environ["SLACK_CHANNEL"]
-        slack_token = os.environ["SLACK_TOKEN"]
-        kafka_topic = os.getenv("KAFKA_TOPIC", "nada.nada-soda")
     except KeyError:
-        logging.error("Environment variables SODA_CONFIG, SODA_CHECKS_FOLDER, SLACK_CHANNEL and SLACK_TOKEN are all required to run this script")
+        logging.error("Environment variables SODA_CONFIG, SODA_CHECKS_FOLDER and SLACK_CHANNEL are all required to run this script")
         exit(1)
     
-    soda_checks = NadaSoda(config_path, checks_path, slack_channel, slack_token, kafka_topic)
+    soda_checks = NadaSoda(config_path, checks_path, slack_channel)
     try:
         soda_checks.run()
     except:
