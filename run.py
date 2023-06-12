5d9459f67861ad55282d61d03885c4dc0797deac
Erik Vattekar <erik.vattekar@nav.no>
2023-06-12 13:42:31 +0200

    disable soda anonymous usage statistics

--- run.py
+++ run.py
@@ -25,12 +25,18 @@ class NadaSoda:
 
     def _run_scan(self, f: str) -> tuple[str, str, Scan]:
         s = Scan()
-        s.add_configuration_yaml_file(file_path=self._soda_config)
+        self._add_configuration_yaml(s, f)
         dataset = f.split(".")[0]
         s.set_data_source_name(dataset)
         s.add_sodacl_yaml_file(f"{self._soda_checks_folder}/{f}")
         s.execute()
         return self._get_gcp_project(dataset), dataset, s
+
+    def _add_configuration_yaml(s: Scan, config_file: str) -> None:
+        with open(config_file, "r") as f:
+            cfg = yaml.safe_load(f.read())
+        cfg["send_anonymous_usage_stats"] = False
+        s.add_configuration_yaml_str(yaml.dump(cfg))
 
     def _get_gcp_project(self, dataset: str) -> str:
         with open(self._soda_config, "r") as f:
